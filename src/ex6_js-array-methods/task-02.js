579e6806f443abd881d4c8eea9e969762d834d2f
IvanNazz <ivan_nazarov_90@list.ru>
2021-04-05 21:49:15 +0300

    fix 1

--- src/ex6_js-array-methods/task-02.js
+++ src/ex6_js-array-methods/task-02.js
@@ -1,7 +1,5 @@
 const some = function(arr, callback, thisArg) {
-    let i, length = arr.length;
-
-    for (i = 0; i < length; i = i + 1) {
+    for (i = 0; i < arr.length; i = i + 1) {
         if (callback.call(thisArg, arr[i], i, arr)) {
         return true;
         }
--- src/ex6_js-array-methods/task-04.js
+++ src/ex6_js-array-methods/task-04.js
@@ -1,7 +1,7 @@
 const filter = function(arr, callback, thisArg) {
-    let i, length = arr.length, results = [];
+    results = [];
 
-    for (i = 0; i < length; i = i + 1) {
+    for (i = 0; i < arr.length; i = i + 1) {
         if (callback.call(thisArg, arr[i], i, arr)) {
         results.push(arr[i]);
         }
