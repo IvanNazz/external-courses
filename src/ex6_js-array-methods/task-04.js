98801ec017283a6d342b506e9dc916307aa5121e
IvanNazz <ivan_nazarov_90@list.ru>
2021-04-06 22:21:08 +0300

    fix 2

--- src/ex6_js-array-methods/task-04.js
+++ src/ex6_js-array-methods/task-04.js
@@ -1,5 +1,5 @@
 const filter = function(arr, callback, thisArg) {
-    results = [];
+    const results = [];
 
     for (i = 0; i < arr.length; i = i + 1) {
         if (callback.call(thisArg, arr[i], i, arr)) {
--- src/ex6_js-array-methods/task-05.js
+++ src/ex6_js-array-methods/task-05.js
@@ -1,6 +1,6 @@
 const map = function(arr, callback, thisArg) {
     
-    results = [];
+    const results = [];
 
     for (i = 0; i < arr.length; i = i + 1) {
         results.push(callback.call(thisArg, arr[i], i, arr));
